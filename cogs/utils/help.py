de1ea8b41cd4310da89ec692ce3c3fb62a40158e
Pyotr K.-Kjellman <pyotr.kkjellman@gmail.com>
2018-04-29 18:39:20 +0200

    Thing

--- cogs/utils/help.py
+++ cogs/utils/help.py
@@ -32,7 +32,7 @@ class HelpFormatter:
         commands = self.cogs[cogname]
         embed = discord.Embed(title=f'{cogname} category:', color=discord.Color.dark_teal())
         for command in commands:
-            embed.add_field(name=command.name, value=command.short_doc, inline=False)
+            embed.add_field(name=command.name, value=command.short_doc if command.short_doc is not None else 'Unavailable', inline=False)
         embed.set_footer(
             text=f'Page {list(self.cogs.keys()).index(cogname) + 1}/{len(self.cogs)}')  # Set embed footer to pagenumber
         sorted(embed.fields, key=attrgetter('name'))  # Sort embed fields by name
