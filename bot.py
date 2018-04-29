727b8819f1b45f875cc56507aaacc7c565a16e92
Pyotr K.-Kjellman <pyotr.kkjellman@gmail.com>
2018-04-29 13:21:31 +0200

    on_ready event and disable commands in dm, ignore bots

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+import discord
+from discord.ext import commands
+import aiohttp
 
+class Boat(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    async def on_ready(self):
+        print('='*10)
+        print(f'Logged in as {self.user} with the id {self.user.id}')
+        print(f'Guild count: {len(self.guilds)}')
+        print('='*10)
+
+    async def on_message(self, message):
+        if not isinstance(message.channel, discord.TextChannel):
+            return
+        if message.author.bot:
+            return
+        
+        await self.process_commands(message)
