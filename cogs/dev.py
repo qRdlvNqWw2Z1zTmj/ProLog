1d4d8056a114d0c5a9db82df850aaf97ba08eb56
ItWasAllIntended <sethwheway101@gmail.com>
2018-04-29 13:42:10 +0100

    Move eval into a cog

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,21 +1,12 @@
 import asyncio
-import io
-import textwrap
 import traceback
-from contextlib import redirect_stdout
 
 from discord.ext import commands
 
 
-class Eval:
+class Dev:
     def __init__(self, bot):
         self.bot = bot
-
-    def cleanup_code(self, content):
-        """Automatically removes code blocks from the code."""
-        # Remove ```py\n```
-        if content.startswith('```') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
 
 
     @commands.command(help='Logs the bot out.', usage='')
@@ -64,52 +55,8 @@ class Eval:
         await ctx.send(f"Successfully reloaded module {module} {ctx.author.mention}")
 
 
-    @commands.command(hidden=True, name='eval')
-    async def _eval(self, ctx, *, body: str):
-        env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            '_': self._last_result
-        }
-
-        env.update(globals())
-
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
-
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                self._last_result = ret
-                await ctx.send(f'```py\n{value}{ret}\n```')
 
 
 
 def setup(bot):
-    bot.add_cog(Eval(bot))
+    bot.add_cog(Dev(bot))
