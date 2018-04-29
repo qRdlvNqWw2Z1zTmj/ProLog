af4818d9c00968746fb4dd1454ffc49a7414b350
ItWasAllIntended <sethwheway101@gmail.com>
2018-04-29 17:23:19 +0100

    Temp

--- cogs/temp.py
+++ cogs/temp.py
@@ -5,6 +5,7 @@ class Temp:
     def __init__(self, bot):
         self.bot = bot
 
+
     @commands.command()
     async def purge(self, ctx, limit: int):
         """Removes a certain amount of messages."""
