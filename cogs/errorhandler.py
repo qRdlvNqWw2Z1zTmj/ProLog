8413bc510984d85ffa94c4d1f729dc1fe5d6d009
Pyotr K.-Kjellman <pyotr.kkjellman@gmail.com>
2018-04-30 14:55:03 +0200

    Removed some whitespace

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -64,7 +64,5 @@ class ErrorHandler:
         print('Ignoring exception in command {ctx.command}:', file=sys.stderr)
         traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
-
-
 def setup(bot):
     bot.add_cog(ErrorHandler(bot))
