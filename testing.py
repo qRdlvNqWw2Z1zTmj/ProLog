ebcbf5d288975ba45211e8ab21b0cb92c0a352b6
Pyotr K.-Kjellman <pyotr.kkjellman@gmail.com>
2018-04-30 19:25:45 +0200

    Separate testing thing

---
+++ testing.py
@@ -0,0 +1,23 @@
+from bot import ProLog
+import config
+import traceback
+import sys
+
+if __name__ == '__main__':
+    # Def bot
+    bot = ProLog(command_prefix=ProLog.prefix)
+
+    # Load cogs
+    for extension in ["cogs.help", "cogs.dev", "cogs.eval", "cogs.general", "cogs.temp", "cogs.errorhandler", "cogs.guildevents"]:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+    # Run bot
+    try:
+        bot.run(config.testingtoken)
+    finally:
+        bot.prefixes.save()
+        bot.prefixes.close()
