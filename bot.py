aeb1e6d2ec16f6c74d6e5e8e406dca4f9186eeb6
Pyotr K.-Kjellman <pyotr.kkjellman@gmail.com>
2018-04-30 15:53:21 +0200

    Command prefix base

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # 1 more
 import sys
 import traceback
+import json
 
 import discord
 from discord.ext import commands
@@ -10,6 +11,13 @@ import config
 
 class ProLog(commands.Bot):
     def __init__(self, *args, **kwargs):
+        try:
+            self.pref_file = open('prefixes.json', 'r')
+        except FileNotFoundError:
+            with open('prefixes.json', 'w') as f:
+                json.dump({}, f)
+            self.pref_file = open('prefixes.json', 'r')
+        self.prefixes = json.load(self.pref_file)
         super().__init__(*args, **kwargs)
 
     async def on_ready(self):
@@ -25,12 +33,21 @@ class ProLog(commands.Bot):
         if message.author.bot:
             return
         await self.process_commands(message)
+        
+    async def prefix(self, message):
+        try:
+            print(self.prefixes)
+            prefixes = self.prefixes[str(message.guild.id)]
+        except KeyError:
+            self.prefixes[str(message.guild.id)] = ['!']
+            print(self.prefixes)
+            prefixes = self.prefixes[str(message.guild.id)]
+        return commands.when_mentioned_or(*prefixes)(self, message)
 
 
 if __name__ == '__main__':
     # Def bot
-    bot = ProLog(command_prefix=['?', "!"])
-    bot.remove_command("help")
+    bot = ProLog(command_prefix=ProLog.prefix)
 
     # Load cogs
     for extension in ["cogs.help", "cogs.dev", "cogs.eval", "cogs.general", "cogs.temp", "cogs.errorhandler", "cogs.guildevents"]:
