3f0ee1cbb3eec3da65fdef9d63955d168a6ba91d
ItWasAllIntended <sethwheway101@gmail.com>
2018-04-30 18:40:15 +0100

    There

--- cogs/utils/functions.py
+++ cogs/utils/functions.py
@@ -4,17 +4,12 @@ from discord.ext.commands import EmojiConverter
 async def completed(message):
     await message.add_reaction("check:440135593037660180")
 
-async def has_emoji(text):
+async def has_emoji(ctx, text):
     words = text.split(' ')
     emojis = []
     for word in words:
-#        emojis.append(is_emoji(word))
-
-    emojis.append(await EmojiConverter().convert())
-
-
+        emojis.append(await is_emoji(ctx, word))
     return len(emojis) != 0
 
-def is_emoji(text):
-    pattern = r':(A-Za-z0-9)?:'
-    return re.search(pattern, text)
+async def is_emoji(ctx, text):
+    return await EmojiConverter().convert(ctx, text)
