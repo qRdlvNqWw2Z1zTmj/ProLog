6d2f5813c6b2d1f6efa2d2d5850c60ffda16770e
Pyotr K.-Kjellman <pyotr.kkjellman@gmail.com>
2018-04-30 19:26:02 +0200

    Broken thign i have no idea how to do please fix seth

--- cogs/utils/functions.py
+++ cogs/utils/functions.py
@@ -1,8 +1,15 @@
+import re
+
 async def completed(message):
     await message.add_reaction("check:440135593037660180")
 
 def has_emoji(text):
-    pass
+    words = text.split(' ')
+    emojis = []
+    for word in words:
+        emojis.append(is_emoji(word))
+    return len(emojis) != 0
 
 def is_emoji(text):
-    pass
+    pattern = r':(A-Za-z0-9)?:'
+    return re.search(pattern, text)
