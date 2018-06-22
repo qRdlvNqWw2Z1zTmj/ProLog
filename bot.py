82ed5b4be60c2f52ee9feb8e90487b8ad1aa30e6
ItWasAllIntended <sethwheway101@gmail.com>
2018-06-22 19:46:08 +0100

    Don't be a sexy silly sausage pY

--- bot.py
+++ bot.py
@@ -2,41 +2,30 @@ import asyncio
 import json
 import sys
 import traceback
-import ssl
+
 import asyncpg
 import discord
 from discord.ext import commands
-import os.path
+
 import config
 from cogs.utils import data
+from cogs.utils import dbfunctions
 
 
 class ProLog(commands.Bot):
     def __init__(self):
         self._cogs = data.cogs
         self.modules = data.modules
-        self._prefix_cog = None
-
         async def prefix(bot, message):
-            if self._prefix_cog is None:
-                self._prefix_cog = self.get_cog('DatabaseFunctions')
-                try:
-                    return await prefix(bot, message)
-                except RecursionError:
-                    print('Prefix stuff broken')
-                    exit()
-            prefixes = await self._prefix_cog.get_prefixes(bot, message)
+            prefixes = await dbfunctions.DatabaseFunctions(self).get_prefixes(bot, message)
             return commands.when_mentioned_or(*prefixes)(bot, message)
-
         super().__init__(command_prefix=prefix)
 
     async def _init(self):
         async def init_connection(conn):
-            await conn.set_type_codec('jsonb', encoder=json.dumps, decoder=json.loads, schema='pg_catalog')
-
+            await conn.set_type_codec("jsonb", encoder=json.dumps, decoder=json.loads, schema="pg_catalog")
         try:
-            self.db = await asyncio.wait_for(
-                asyncpg.create_pool(config.postgresql, init=init_connection), 10)
+            self.db = await asyncio.wait_for(asyncpg.create_pool(config.postgresql, init=init_connection), 10)
         except Exception as e:
             print("Could not conntect not Postgres databse. Exiting", file=sys.stderr)
             print(e)
@@ -49,15 +38,15 @@ class ProLog(commands.Bot):
             try:
                 self.load_extension(extension)
             except Exception as e:
-                print(f'Failed to load extension {extension}.', file=sys.stderr)
+                print(f"Failed to load extension {extension}.", file=sys.stderr)
                 traceback.print_exc()
 
-        print('=' * 10)
-        print(f'Logged in as {self.user} with the id {self.user.id}')
+        print("=" * 10)
+        print(f"Logged in as {self.user} with id {self.user.id}")
         print("Logged into PostgresSQL server")
         print(f"Loaded cogs {', '.join(self.cogs)}")
-        print(f'Guild count: {len(self.guilds)}')
-        print('=' * 10)
+        print(f"Guild count: {len(self.guilds)}")
+        print("=" * 10)
 
     async def on_message(self, message):
         if not isinstance(message.channel, discord.TextChannel) or message.author.bot:
@@ -65,7 +54,7 @@ class ProLog(commands.Bot):
         await self.process_commands(message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot = ProLog()
     bot.run(config.token)
 
