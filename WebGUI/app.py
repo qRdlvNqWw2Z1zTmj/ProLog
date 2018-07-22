dc85c3ed8836ed09c53a2c50e7092f5366c1b682
ItWasAllIntended <sethwheway101@gmail.com>
2018-07-22 18:42:28 +0100

    Quart over flask + new requirements

--- WebGUI/app.py
+++ WebGUI/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from quart import Quart
 
-app = Flask(__name__)
+app = Quart(__name__)
 
 
 @app.route('/')
-def hello_world():
+async def hello_world():
     return 'Hello World!'
 
 
