287207b43309008af0116d594c060aaf081542d3
ItWasAllIntended <sethwheway101@gmail.com>
2018-08-02 10:01:57 +0100

    Functions now make a little more sense

--- cogs/utils/database.py
+++ cogs/utils/database.py
@@ -1,31 +1,32 @@
 from discord.ext import commands
 
-from cogs.utils.cache import cached_function
-
 
 class DatabaseFunctions:
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_row(self, guildid: int, dbtable, dbcolumn, key=None):
-        result = await self.bot.db.fetchrow(f"""
-            SELECT {dbcolumn} FROM {dbtable} 
-            WHERE guildid = $1
-            """, guildid)
-        if key is not None:
-            try:
-                return result[key]
-            except:
-                return result
-        return result
-
     async def get_prefixes(self, bot, message):
-        prefixes = await self.get_row(message.guild.id, "configs", "prefixes", "prefixes")
+        prefixes = await self.fetch_value(message.guild.id, "configs", "prefixes")
         if prefixes is None:
             await self.bot.db.execute("INSERT INTO configs(guildid, prefixes) VALUES($1, '{!}')", message.guild.id)
             # self.get_prefixes.invalidate(self.get_prefixes.get_id(self.bot, message))     CACHE FIX
-            prefixes = await self.get_row(message.guild.id, "configs", "prefixes", "prefixes")
+            prefixes = await self.fetch_value(message.guild.id, "configs", "prefixes")
+        print(prefixes)
         return commands.when_mentioned_or(*prefixes)(bot, message)
+
+    async def fetch_row(self, guildid, dbtable):
+        result = await self.bot.db.fetchrow(f"""
+            SELECT * FROM {dbtable} 
+            WHERE guildid = $1
+            """, guildid)
+        return result
+
+    async def fetch_value(self, guildid, dbtable, dbcolumn):
+        result = await self.bot.db.fetchval(f"""
+        SELECT {dbcolumn} FROM {dbtable}
+        WHERE guildid = $1
+        """, guildid)
+        return result
 
     async def set_item(self, guildid: int, dbtable, dbcolumn, value):
         async with self.bot.db.acquire() as connection:
